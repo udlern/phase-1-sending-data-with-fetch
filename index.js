efc43b837d6b4524c717eb754be84a946b94a609
Noa Udler <udlernoa4@gmail.com>
2021-11-11 09:43:45 -0800

    commit tree 61be300e9ce3cc215f714ac5410ba68beb0240d4

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Add your code here
+function submitData(userName, userEmail) {
+  const userNameAndEmail = { name: userName, email: userEmail };
+  const configurationObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(userNameAndEmail),
+  };
+   return fetch("http://localhost:3000/users", configurationObject)
+    .then(resp => resp.json())
+    .then((userNameAndEmail) => document.body.append(userNameAndEmail.id))
+    .catch(error => document.body.append(error.message));
+}
